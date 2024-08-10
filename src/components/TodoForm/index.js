47b6d46e3cb2d0e1353a5750aecb7fe8723ee633
Esteban <joelpardo9@gmail.com>
2024-08-10 18:13:42 -0300

    Se agrega validacion de todos

--- src/components/TodoForm/index.js
+++ src/components/TodoForm/index.js
@@ -6,16 +6,29 @@ import "./TodoForm.css";
 function TodoForm() {
   const { addTodo, setOpenModal } = React.useContext(TodoContext);
   const [newTodoValue, setNewTodoValue] = React.useState("");
+  const [showError, setShowError] = React.useState(false); 
+
+
   const onSubmit = (event) => {
     event.preventDefault();
-    addTodo(newTodoValue);
-    setOpenModal(false);
+    if(newTodoValue.length > 0)
+    {
+      addTodo(newTodoValue);
+      setOpenModal(false);
+      setShowError(false);
+    } else {
+      setShowError(true); // Muestra el error si no se ha ingresado un TODO
+    }     
+ 
   };
   const onCancel = () => {
     setOpenModal(false);
   };
   const onChange = (event) => {
     setNewTodoValue(event.target.value);
+    if (event.target.value.length > 0) {
+      setShowError(false); // Esconde el error mientras se escribe
+    }
   };
   return (
     <form onSubmit={onSubmit}>
@@ -34,9 +47,17 @@ function TodoForm() {
           Cancelar
         </button>
         <button type="submit" className="TodoForm-button TodoForm-button--add">
-          AÃ±adir
+          Agregar
         </button>
+        
       </div>
+      <div>
+        <label className={`TodoForm-error-message ${showError ? '' : 'oculto'}`}>
+          Debe ingresar TODO
+        </label>
+      </div>
+
+    
     </form>
   );
 }
